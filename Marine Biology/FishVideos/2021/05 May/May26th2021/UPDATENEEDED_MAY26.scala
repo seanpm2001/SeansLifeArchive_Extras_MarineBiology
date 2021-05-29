2e22af4bedcc92d681435aca0e73c4afb246a8e5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-28 22:16:22 -0700

    Data from May 26th 2021 (4 files)
    1 sub-directory for this directory, containing 4 files in Markdown and Scala formats. This is all the data from May 26th 2021.

---
+++ Marine Biology/FishVideos/2021/05 May/May26th2021/UPDATENEEDED_MAY26.scala
@@ -0,0 +1,5 @@
+// Update needed
+object updateNeeded extends Application { // Start of object
+  println("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted)") // Message body
+} // End of object
+//EOF
