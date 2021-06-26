46e5d9dd998a59423ba1d7c4cb2de7c53eddd061
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-25 21:20:42 -0700

    Data from June 22nd 2021 (1 sub-directory, 5 files)
    1 sub-directory for this directory, containing 5 files in Markdown, PowerShell, and Scala formats. This is all the data from June 22nd 2021.

---
+++ Marine Biology/FishVideos/2021/06 June/June22nd2021/UPDATENEEDED_JUNE22ND2021.ps1
@@ -0,0 +1,15 @@
+# Start of script
+# Subject: Update needed
+function dailyDescription ([Type]$Param1, [Type]$Param2)
+{
+  # Instructions
+  [Console]::WriteLine("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article.")
+} # End of function
+return dailyDescription()
+# EOF
+# File info
+# File type: PowerShell script (*.ps1)
+# Original file version: 1 (Wednesday, June 2nd 2021 at 9:52 pm)
+# Line count (including blank lines and compiler line): 15
+# End of script
+
