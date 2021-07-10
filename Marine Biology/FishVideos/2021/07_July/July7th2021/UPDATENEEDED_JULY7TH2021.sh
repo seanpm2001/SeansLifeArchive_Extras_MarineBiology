690d03ed0dd8594a2081f3a19a68b06a07952145
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-09 21:05:39 -0700

    Data from July 7th 2021 (1 sub-directory, 4 files)
    1 sub-directory for this directory, containing 4 files in Markdown and Shell formats. This is all the data from July 7th 2021.

---
+++ Marine Biology/FishVideos/2021/07_July/July7th2021/UPDATENEEDED_JULY7TH2021.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Start of script
+# Subject: Update needed
+  # Instructions
+  echo "If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article."
+# EOF
+# File info
+# File type: BASH Shell script (*.sh)
+# Original file version: 1 (Saturday, July 3rd 2021 at 10:35 pm)
+# Line count (including blank lines and compiler line): 12
+# End of script
+
