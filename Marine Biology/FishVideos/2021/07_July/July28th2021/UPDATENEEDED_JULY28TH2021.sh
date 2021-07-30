b10f5e54c43641d3d435af3ade74782745527d3e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-29 21:01:10 -0700

    Data from July 28th 2021 (1 sub-directory, 4 files)
    1 sub-directory for this directory, containing 4 files in Markdown and Shell formats. This is all the data from July 28th 2021.

---
+++ Marine Biology/FishVideos/2021/07_July/July28th2021/UPDATENEEDED_JULY28TH2021.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Start of script
+# Subject: Update needed
+  # Instructions
+mega() {
+  instructions() {
+  	ls /home/seanpm2001/Desktop/MyLifeStory/Milestone5/Extras/MarineBiology/ # Lists the data of the marine biology folder
+  	# dir ..
+  	cal -7 2021 # Prints the calendar for this month in 2021
+  	cal 2021 # Prints the calendar for 2021
+  	lslocks()
+  	echo "If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article."
+  }
+  return instructions()
+  echo "This is pretty much a vanity file now. It doesn't serve much purpose, other than tradition. Its documentation needs to be moved elsewhere"
+  break
+  #TODO
+  # This file is basically a vanity file now, and its documentation needs to be moved elsewhere
+}
+return mega()
+exit
+# EOF
+# File info
+# File type: BASH (Bourne Again SHell script) Shell script (*.sh)
+# Original file version: 1 (Monday, July 12th 2021 at 8:14 pm)
+# Line count (including blank lines and compiler line): 28
+# End of script
+
