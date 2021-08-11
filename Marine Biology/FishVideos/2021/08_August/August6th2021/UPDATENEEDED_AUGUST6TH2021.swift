b0ab497efc92baf8ab2e78132e5886e04bf5279c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-10 21:32:51 -0700

    Data from August 6th 2021 (1 sub-directory, 4 files)
    1 sub-directory for this directory, containing 4 files in Markdown, and Swift formats. This is all the data from August 6th 2021.

---
+++ Marine Biology/FishVideos/2021/08_August/August6th2021/UPDATENEEDED_AUGUST6TH2021.swift
@@ -0,0 +1,22 @@
+// Start of script
+// Subject: Update needed
+  // Instructions
+mega() {
+  instructions() {
+  	NSString str = message1("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article.");
+}
+  return message1();
+  NSString str = message2("This is pretty much a vanity file now. It doesn't serve much purpose, other than tradition. Its documentation needs to be moved elsewhere"
+  return message2();
+  break
+  //TODO
+  // This file is basically a vanity file now, and its documentation needs to be moved elsewhere
+}
+return mega()
+// EOF
+// File info
+// File type: Apple Swift source file (*.swift)
+// Original file version: 1 (Thursday, August 5th 2021 at 9:15 pm)
+// Line count (including blank lines and compiler line): 22
+// End of script
+
