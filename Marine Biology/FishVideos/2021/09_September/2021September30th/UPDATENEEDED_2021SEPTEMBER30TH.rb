1aee902fcfbc73ab00608ac1a4a8eb2650ca3749
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-01 21:58:22 -0700

    Data from 2021 September 30th (1 sub-directory, 4 files)
    1 sub-directory for this directory, containing 4 files in Markdown and Ruby formats. This is all the data from 2021 September 30th.

---
+++ Marine Biology/FishVideos/2021/09_September/2021September30th/UPDATENEEDED_2021SEPTEMBER30TH.rb
@@ -0,0 +1,27 @@
+# Start of script
+# Subject: Update needed
+  # Instructions
+class vanity() {
+def mega() { # Very redundant, made as complicated as possible
+	def instructions() {
+  		echo ("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article.");
+		echo ("This is pretty much a vanity file now. It doesn't serve much purpose, other than tradition. Its documentation needs to be moved elsewhere");
+  		return instructions();
+  		break
+  	}
+  #TODO
+  # This file is basically a vanity file now, and its documentation needs to be moved elsewhere
+	break
+}
+return mega();
+break;
+}
+# EOF
+
+# File info
+# File type: Ruby 3.0 source file (*.rb)
+# Original file version: 1 (Wednesday, 2021 September 1st at 10:19 pm)
+# Line count (including blank lines and compiler line): 27
+
+# End of script
+
