c284db08c6693c9607894faeb1fb3389c7ae27ad
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-18 21:27:30 -0700

    Data from 2021 October 15th (1 sub-directory, 4 files)
    1 sub-directory for this directory, containing 4 files in Markdown and Perl formats. This is all the data from 2021 October 15th.

---
+++ Marine Biology/FishVideos/2021/10_October/2021October15th/UPDATENEEDED_2021OCTOBER15TH.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# Start of script
+# Subject: Update needed
+# Instructions
+# Modules used
+use strict;
+use warnings;
+
+# Print function
+continue;
+print("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article.\n");
+print("This is pretty much a vanity file now. It doesn't serve much purpose, other than tradition. Its documentation needs to be moved elsewhere\n");
+break;
+exit;
+# EOF
+
+# File info
+# File type: Perl 5.0 source file (*.pl)
+# Original file version: 1 (Saturday, 2021 October 2nd at 9:53 pm)
+# Line count (including blank lines and compiler line): 23
+
+# End of script
+
