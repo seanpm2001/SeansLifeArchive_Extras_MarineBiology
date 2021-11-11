12510da5dc9fee9a10145e6e7a6a5d66cec9ab58
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-11 00:24:22 -0800

    Data from 2021 November 10th (1 sub-directory, 7 files)
    1 sub-directory for this directory, containing 7 files in Markdown, Scala, YML, and YAML formats. This is all the data from 2021 November 10th.

---
+++ Marine Biology/FishVideos/2021/11_November/2021November10th/UPDATENEEDED_2021NOVEMBER10TH..scala
@@ -0,0 +1,36 @@
+//!/usr/bin/perl
+// Start of script
+// Subject: Update needed
+// Instructions
+// Modules used
+/*use strict;
+use warnings;
+*/
+// Print function
+// continue;
+object UNEED_PROJECT-MONTH-LANG extends Application {
+	println("Project month language")
+  	println("If a description has not been added already, please write a description about the video (the length, size, purpose, and uniqueness should be noted). These notes are not too relevant anymore, but are kept out of tradition. They are a reminder to write a description, instead of just having a direct copy and paste without the relevant info into the next days article.\n")
+	println("This is pretty much a vanity file now. It doesn't serve much purpose, other than tradition. Its documentation needs to be moved elsewhere\n");
+}
+return object(UNEED_PROJECT-MONTH-LANG).++
+return 0
+var x = "0"
+return 0 + x
+x(*)-/\\* { &
+^$64/32\\ }{ /0
+% & &16& if x = 0
+{ // Beautiful colors and patterns
+} // Sloping down this hill. We have plataued. EEEEE EEEEE EEEEE EEEEE EE-
+// break;
+// exit;
+// EOF
+
+// File info
+
+// File type: Perl 5.0 source file (*.pl)
+// Original file version: 1 (Tuesday, 2021 November 2nd at 11:05 pm)
+// Line count (including blank lines and compiler line): 36
+
+// End of script
+
